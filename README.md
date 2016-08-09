398b6b630c3521a0f4a92c17c3356ba05a9849dc
iitow <iitow@isilon.com>
2016-08-08 17:16:28 -0700

    Update docs

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
+## Install
+```
+python setup.py install
+or.
+pip install bstree
+```
+
+## Usage
+```
+optional arguments:
+  -h, --help   show this help message and exit
+  -o ORIGIN    Origin tree, base path
+  -n NEW       New tree, base path
+  -p PATH      absolute path to place output
+  -e EXCLUDES  exclude files or directories
+  -d           enable debug messages
+  --version    show program's version number and exit
+```
+## example
+```
+cd ~
+mkdir -p origin
+cd origin touch apples.txt
+echo "hello world" > apples.txt
+mkdir -p new
+cd origin touch apples.txt
+echo "findme I`m different" > apples.txt
+mkdir -p files
+bstree -o ~/origin -n ~/new -p ~/files
+```
+
 
 **********************************************
  File @ ***/main.py***
--- setup.py
+++ setup.py
@@ -4,25 +4,20 @@ Created on Apr 29, 2016
 @author: iitow
 '''
 from setuptools import setup, find_packages
-
 SRCDIR = 'src'
-
-
 def readme():
     ''' Spits out README.rst for our long_description
     with open('README.rst', 'r') as fobj:
         return fobj.read()
     '''
-
-
 setup(
     name='bstree',
     version='1.0.1',
-    description="find binaries which differ between directory trees of similar layout. Systems [Freebsd, Linux, OSX]",
+    description="diff all files and executables in 2 trees provides a list of files/executables changed. Systems [Freebsd, Linux, OSX]",
     long_description=readme(),
     author='Ian Itow',
     author_email='itow0001@gmail.com',
-    url='https://github.com/itow0001/pytomd',
+    url='https://github.com/itow0001/bsdiff-tree',
     license='MIT',
     classifiers=[
         'Topic :: Software Development :: Build Tools',
